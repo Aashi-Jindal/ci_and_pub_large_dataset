75e60792f5b4e39c2d74bb73b4588685e7a61fb0
Aashi-Jindal <jindal.aashi21@gmail.com>
2025-07-14 12:27:52 +0530

    mkdir datasets in fetch large dataset shell

--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -3,6 +3,7 @@
 TRAINING_DATA_URL="vbookshelf/v2-plant-seedlings-dataset"
 NOW=$(date)
 
+mkdir -p model_package/cnn_model/datasets/
 kaggle datasets download -d $TRAINING_DATA_URL -p model_package/cnn_model/datasets/ && \
 unzip model_package/cnn_model/datasets/v2-plant-seedlings-dataset.zip -d model_package/cnn_model/datasets/v2-plant-seedlings-dataset && \
 echo $TRAINING_DATA_URL 'retrieved on:' $NOW > model_package/cnn_model/datasets/training_data_reference.txt && \
